836885ce8cb6bcdf1f828e25bce416f7143193aa
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 21:17:00 +0530

    Update shell.py

--- Loverbot/modules/shell.py
+++ Loverbot/modules/shell.py
@@ -1,7 +1,7 @@
 import subprocess
 
-from Razerbot import LOGGER, dispatcher
-from Razerbot.modules.helper_funcs.chat_status import dev_plus
+from Loverbot import LOGGER, dispatcher
+from Loverbot.modules.helper_funcs.chat_status import dev_plus
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, CommandHandler
 from telegram.ext.dispatcher import run_async
