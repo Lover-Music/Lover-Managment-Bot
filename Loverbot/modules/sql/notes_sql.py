7f6208d6541d5234b2587a1844d23eef117ff1df
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 16:09:38 +0530

    Update notes_sql.py

--- Loverbot/modules/sql/notes_sql.py
+++ Loverbot/modules/sql/notes_sql.py
@@ -1,8 +1,8 @@
 # Note: chat_id's are stored as strings because the int is too large to be stored in a PSQL database.
 import threading
 
-from Razerbot.modules.helper_funcs.msg_types import Types
-from Razerbot.modules.sql import BASE, SESSION
+from Loverbot.modules.helper_funcs.msg_types import Types
+from Loverbot.modules.sql import BASE, SESSION
 from sqlalchemy import Boolean, Column, BigInteger, String, UnicodeText, distinct, func
 
 class Notes(BASE):
