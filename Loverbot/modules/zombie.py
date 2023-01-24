504ab4f17a936f61849baf74b665dc230aed73f5
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 22:31:48 +0530

    Update zombie.py

--- Loverbot/modules/zombie.py
+++ Loverbot/modules/zombie.py
@@ -6,7 +6,7 @@ from telethon.errors import ChatAdminRequiredError, UserAdminInvalidError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.types import ChatBannedRights, ChannelParticipantsAdmins
 
-from Razerbot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
+from Loverbot import telethn, OWNER_ID, DEV_USERS, DRAGONS, DEMONS
 
 # =================== CONSTANT ===================
 
