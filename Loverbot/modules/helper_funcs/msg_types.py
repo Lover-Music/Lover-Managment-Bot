93fa1fd11d0baed75596758a00b1cf5fb7857817
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 13:11:39 +0530

    Update msg_types.py

--- Loverbot/modules/helper_funcs/msg_types.py
+++ Loverbot/modules/helper_funcs/msg_types.py
@@ -1,6 +1,6 @@
 from enum import IntEnum, unique
 
-from Razerbot.modules.helper_funcs.string_handling import button_markdown_parser
+from Loverbot.modules.helper_funcs.string_handling import button_markdown_parser
 from telegram import Message
 
 
