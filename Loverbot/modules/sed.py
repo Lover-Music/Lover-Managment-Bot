5a0f6a73b65240a601d1d93745d02ae7d17e0d5f
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 21:15:01 +0530

    Update sed.py

--- Loverbot/modules/sed.py
+++ Loverbot/modules/sed.py
@@ -2,9 +2,9 @@ import sre_constants
 
 import regex
 import telegram
-from Razerbot import LOGGER, dispatcher
-from Razerbot.modules.disable import DisableAbleMessageHandler
-from Razerbot.modules.helper_funcs.regex_helper import infinite_loop_check
+from Loverbot import LOGGER, dispatcher
+from Loverbot.modules.disable import DisableAbleMessageHandler
+from Loverbot.modules.helper_funcs.regex_helper import infinite_loop_check
 from telegram import Update
 from telegram.ext import CallbackContext, Filters, run_async
 
