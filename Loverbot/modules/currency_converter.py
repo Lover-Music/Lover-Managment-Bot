0f79822ddce07a89449335623e9bfcaca71c7100
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 16:51:43 +0530

    Update currency_converter.py

--- Loverbot/modules/currency_converter.py
+++ Loverbot/modules/currency_converter.py
@@ -1,5 +1,5 @@
 import requests
-from Razerbot import CASH_API_KEY, dispatcher
+from Loverbot import CASH_API_KEY, dispatcher
 from telegram import Update, ParseMode
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
