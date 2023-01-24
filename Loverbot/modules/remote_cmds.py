e141eeffbc3e6dfae9ff8fe1c73eb41011165395
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 21:06:04 +0530

    Update remote_cmds.py

--- Loverbot/modules/remote_cmds.py
+++ Loverbot/modules/remote_cmds.py
@@ -1,12 +1,12 @@
-from Razerbot import dispatcher
-from Razerbot.modules.helper_funcs.chat_status import (
+from Loverbot import dispatcher
+from Loverbot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_bot_admin,
     is_user_ban_protected,
     is_user_in_chat,
 )
-from Razerbot.modules.helper_funcs.extraction import extract_user_and_text
-from Razerbot.modules.helper_funcs.filters import CustomFilters
+from Loverbot.modules.helper_funcs.extraction import extract_user_and_text
+from Loverbot.modules.helper_funcs.filters import CustomFilters
 from telegram import Update, ChatPermissions
 from telegram.error import BadRequest
 from telegram.ext import CallbackContext, CommandHandler, run_async
