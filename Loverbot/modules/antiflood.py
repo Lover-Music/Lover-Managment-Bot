ffbe9cba82c067d934e74b52184a754632ad97e3
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 16:29:13 +0530

    Update antiflood.py

--- Loverbot/modules/antiflood.py
+++ Loverbot/modules/antiflood.py
@@ -4,15 +4,15 @@ import re
 
 from telegram import Message, Chat, Update, User, ChatPermissions
 
-from Razerbot import TIGERS, WOLVES, dispatcher
-from Razerbot.modules.helper_funcs.chat_status import (
+from Loverbot import TIGERS, WOLVES, dispatcher
+from Loverbot.modules.helper_funcs.chat_status import (
     bot_admin,
     is_user_admin,
     user_admin,
     user_admin_no_reply,
 )
-from Razerbot.modules.log_channel import loggable
-from Razerbot.modules.sql import antiflood_sql as sql
+from Loverbot.modules.log_channel import loggable
+from Loverbot.modules.sql import antiflood_sql as sql
 from telegram.error import BadRequest
 from telegram.ext import (
     CallbackContext,
@@ -23,10 +23,10 @@ from telegram.ext import (
     run_async,
 )
 from telegram.utils.helpers import mention_html, escape_markdown
-from Razerbot.modules.helper_funcs.string_handling import extract_time
-from Razerbot.modules.connection import connected
-from Razerbot.modules.helper_funcs.alternate import send_message
-from Razerbot.modules.sql.approve_sql import is_approved
+from Loverbot.modules.helper_funcs.string_handling import extract_time
+from Loverbot.modules.connection import connected
+from Loverbot.modules.helper_funcs.alternate import send_message
+from Loverbot.modules.sql.approve_sql import is_approved
 
 FLOOD_GROUP = 3
 
