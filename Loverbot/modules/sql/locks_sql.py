f342efcd9008a8b3ba4185a38b86aaa5f1dbe6bf
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 15:40:21 +0530

    Update locks_sql.py

--- Loverbot/modules/sql/locks_sql.py
+++ Loverbot/modules/sql/locks_sql.py
@@ -3,7 +3,7 @@ import threading
 
 from sqlalchemy import Column, String, Boolean
 
-from Razerbot.modules.sql import SESSION, BASE
+from Loverbot.modules.sql import SESSION, BASE
 
 
 class Permissions(BASE):
