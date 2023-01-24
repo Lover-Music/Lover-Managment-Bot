9c2dc1b6ac46a83f484e77327a8586a6666eecca
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 21:19:52 +0530

    Update speed_test.py

--- Loverbot/modules/speed_test.py
+++ Loverbot/modules/speed_test.py
@@ -1,7 +1,7 @@
 import speedtest
-from Razerbot import DEV_USERS, dispatcher
-from Razerbot.modules.disable import DisableAbleCommandHandler
-from Razerbot.modules.helper_funcs.chat_status import dev_plus
+from Loverbot import DEV_USERS, dispatcher
+from Loverbot.modules.disable import DisableAbleCommandHandler
+from Loverbot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, ParseMode, Update
 from telegram.ext import CallbackContext, CallbackQueryHandler, run_async
 
