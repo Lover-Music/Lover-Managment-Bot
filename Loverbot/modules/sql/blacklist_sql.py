9962c30f19738ec97b608ecddfd5e631da3f241b
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 13:20:13 +0530

    Update blacklist_sql.py

--- Loverbot/modules/sql/blacklist_sql.py
+++ Loverbot/modules/sql/blacklist_sql.py
@@ -2,7 +2,7 @@ import threading
 
 from sqlalchemy import func, distinct, Column, String, UnicodeText, BigInteger
 
-from Razerbot.modules.sql import SESSION, BASE
+from Loverbot.modules.sql import SESSION, BASE
 
 class BlackListFilters(BASE):
     __tablename__ = "blacklist"
