fdb93960740149fd05202d929e0ada4ebfbe1b21
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 16:54:57 +0530

    Update debug.py

--- Loverbot/modules/debug.py
+++ Loverbot/modules/debug.py
@@ -5,8 +5,8 @@ from telethon import events
 from telegram import Update
 from telegram.ext import CallbackContext, CommandHandler, run_async
 
-from Razerbot import telethn, dispatcher
-from Razerbot.modules.helper_funcs.chat_status import dev_plus
+from Lovererbot import telethn, dispatcher
+from Lovbot.modules.helper_funcs.chat_status import dev_plus
 
 DEBUG_MODE = False
 
