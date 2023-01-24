ba9159b38c4bcd6a984ca537f5503dea9d185afc
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 13:27:01 +0530

    Update chatbot_sql.py

--- Loverbot/modules/sql/chatbot_sql.py
+++ Loverbot/modules/sql/chatbot_sql.py
@@ -2,48 +2,48 @@ import threading
 
 from sqlalchemy import Column, String
 
-from Razerbot.modules.sql import BASE, SESSION
+from Loverbot.modules.sql import BASE, SESSION
 
 
-class RazerChats(BASE):
-    __tablename__ = "razer_chats"
+class LoverChats(BASE):
+    __tablename__ = "lover_chats"
     chat_id = Column(String(14), primary_key=True)
 
     def __init__(self, chat_id):
         self.chat_id = chat_id
 
 
-RazerChats.__table__.create(checkfirst=True)
+LoverChats.__table__.create(checkfirst=True)
 INSERTION_LOCK = threading.RLock()
 
 
-def is_razer(chat_id):
+def is_lover(chat_id):
     try:
-        chat = SESSION.query(RazerChats).get(str(chat_id))
+        chat = SESSION.query(LoverChats).get(str(chat_id))
         return bool(chat)
     finally:
         SESSION.close()
 
 
-def set_razer(chat_id):
+def set_lover(chat_id):
     with INSERTION_LOCK:
-        razerchat = SESSION.query(RazerChats).get(str(chat_id))
-        if not razerchat:
-            razerchat = RazerChats(str(chat_id))
-        SESSION.add(razerchat)
+        razerchat = SESSION.query(loverChats).get(str(chat_id))
+        if not loverrchat:
+            loverchat = LoverChats(str(chat_id))
+        SESSION.add(loverchat)
         SESSION.commit()
 
 
-def rem_razer(chat_id):
+def rem_lover(chat_id):
     with INSERTION_LOCK:
-        razerchat = SESSION.query(RazerChats).get(str(chat_id))
-        if razerchat:
-            SESSION.delete(razerchat)
+        loverchat = SESSION.query(LoverChats).get(str(chat_id))
+        if loverchat:
+            SESSION.delete(loverchat)
         SESSION.commit()
 
 
-def get_all_razer_chats():
+def get_all_lover_chats():
     try:
-        return SESSION.query(RazerChats.chat_id).all()
+        return SESSION.query(LoverChats.chat_id).all()
     finally:
         SESSION.close()
