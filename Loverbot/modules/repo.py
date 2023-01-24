9728b1e0cfe00af80e1f0084cc2b5f66e8f7b624
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 21:10:04 +0530

    Update repo.py

--- Loverbot/modules/repo.py
+++ Loverbot/modules/repo.py
@@ -4,7 +4,7 @@ from pyrogram import __version__ as z
 from telethon import __version__ as s
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram import filters
-from Razerbot import pbot, START_IMG
+from Loverbot import pbot, START_IMG
 
 @pbot.on_message(filters.command(["repo", "source"]))
 async def repo(_, message):
@@ -12,20 +12,20 @@ async def repo(_, message):
         photo=START_IMG,
         caption=f"""ʜᴇʏ ɪ'ᴍ RΛZΞR
 
-⥤ ᴅᴇᴠᴇʟᴏᴘᴇʀ : @WH0907
+⥤ ᴅᴇᴠᴇʟᴏᴘᴇʀ : @shubhamsah1
 ⥤ ᴘʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : `{y()}`
 ⥤ ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ : `{o}`
 ⥤ ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ : `{s}`
 ⥤ ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ : `{z}`
 
-ʀᴀᴢᴇʀʙᴏᴛ ɪs ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ʙᴏᴛ ᴘʀᴏᴊᴇᴄᴛ.
+LOVERBOT ɪs ᴀɴ ᴏᴘᴇɴ sᴏᴜʀᴄᴇ ʙᴏᴛ ᴘʀᴏᴊᴇᴄᴛ.
 ᴄʟɪᴄᴋ ʙᴇʟᴏᴡ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ.
 """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
-                    InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", url="https://github.com/LinuxGuy312/RazerBot"), 
-                    InlineKeyboardButton(text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=1413518510)
+                    InlineKeyboardButton(text="sᴏᴜʀᴄᴇ", url="https://github.com/Lover-Music/Lover-Managment-Bot"), 
+                    InlineKeyboardButton(text="ᴅᴇᴠᴇʟᴏᴘᴇʀ", user_id=1548904516)
                 ]
             ]
         )
