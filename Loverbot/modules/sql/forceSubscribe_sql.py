ef8745b46e6e2db2260a233e3c993df65dadde60
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-24 13:34:07 +0530

    Update forceSubscribe_sql.py

--- Loverbot/modules/sql/forceSubscribe_sql.py
+++ Loverbot/modules/sql/forceSubscribe_sql.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, Numeric, Boolean
-from Razerbot.modules.sql import BASE, SESSION
+from Loverbot.modules.sql import BASE, SESSION
 
 
 class forceSubscribe(BASE):
