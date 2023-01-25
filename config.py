dba17776a7265e6d08618e8f7a2d65e989a6a13a
Lover-Music <123320837+Lover-Music@users.noreply.github.com>
2023-01-25 09:06:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class Config(object):
 
     # RECOMMENDED
     MONGO_DB_URI = "mongodb+srv://2004:2004@cluster0.vugmi1n.mongodb.net/?retryWrites=true&w=majority" 
-    SQLALCHEMY_DATABASE_URI = "postgres://hjhlfxhp:GxZqXL48l1xBq44F8uaCONfraHpUa399@baasu.db.elephantsql.com/hjhlfxhp"  # needed for any database module
+    SQLALCHEMY_DATABASE_URI = "postgres://uxiiwjhu:cZFxP9hsfKeKckaPP0clbNls19cEDy7O@baasu.db.elephantsql.com/uxiiwjhu"  # needed for any database module
     LOAD = []
     NO_LOAD = ["rss", "cleaner", "connection", "math"]
     WEBHOOK = False
